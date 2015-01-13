6d81c281599ca73a9d6eb2b92b177010cf9765d3
unknown <2080613M@BO706-06.ad.dcs.gla.ac.uk>
2015-01-13 14:17:49 +0000

    Chapter 4 - completed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("<p>Rango says hey there world!</ p><p>A link to the <a href=\"about\">about</ a> page.</ p>")
+    return HttpResponse("""<p>Rango says hey there world!</ p>
+    <p>A link to the <a href=\"/rango/about\">about</ a> page.</ p>""")
 
 def about(request):
-    return HttpResponse("""<p>This is the about page.</ p><p>This tutorial has been put together by Seoras Macdonald,
-     2080613</ p><p>A link to the <a href=\"..\">index</ a> page.</ p>""")
+    return HttpResponse("""<p>This tutorial has been put together by Seoras Macdonald,
+     2080613</ p><p>A link to the <a href=\"/rango\">index</ a> page.</ p>""")
